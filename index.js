e6779f1bf9fca8c9f37789c95e1b22d8a5c07815
yuksel.ozdemir <yuksel.ozdemir@dolap.com>
2022-06-12 16:02:44 +0300

    03 - add http methods to index.js

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/member', (req, res) => {
+  res.send('GET')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/member', (req, res) => {
+    res.send('POST')
 })
+
+app.put('/member', (req, res) => {
+    res.send('PUT')
+})
+
+
+app.delete('/member', (req, res) => {
+    res.send('Got a DELETE request at /user')
+})
+
+ 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
