b8d234346ecd95a0c3b8841263ee5f938a958e86
avahmetozdemir <avamertozdemir@gmail.com>
2022-06-16 19:07:48 +0300

    08 -  add cookie-parser example

---
+++ cookie-validator.js
@@ -0,0 +1,7 @@
+module.exports = async function cookieValidator (cookies) {
+    try {
+      await externallyValidateCookie(cookies.testCookie)
+    } catch {
+      throw new Error('Invalid cookies')
+    }
+  }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cookieParser = require('cookie-parser')
+const cookieValidator = require('cookie-validator')
 app.use(express.static('public'))
 
 ///Routes
@@ -24,26 +26,41 @@ app.use(express.static('public'))
  
 //Middleware
 
-const myLogger = function (req, res, next) {
-    console.log('LOGGED')
+// const myLogger = function (req, res, next) {
+//     console.log('LOGGED')
+//     next()
+//   }
+
+// app.use(myLogger)
+
+// const requestTime = function (req, res, next) {
+//     req.requestTime = Date.now()
+//     next()
+//   }
+
+//   app.use(requestTime)
+
+//   app.get('/', (req, res) => {
+//     let responseText = 'Hello World!<br>'
+//     responseText += `<small>Requested at: ${req.requestTime}</small>`
+//     res.send(responseText)
+//   })   
+
+
+
+async function validateCookies (req, res, next) {
+    await cookieValidator(req.cookies)
     next()
   }
-
-app.use(myLogger)
-
-const requestTime = function (req, res, next) {
-    req.requestTime = Date.now()
-    next()
-  }
-
-  app.use(requestTime)
-
-  app.get('/', (req, res) => {
-    let responseText = 'Hello World!<br>'
-    responseText += `<small>Requested at: ${req.requestTime}</small>`
-    res.send(responseText)
-  })   
-
+  
+  app.use(cookieParser())
+  
+  app.use(validateCookies)
+  
+  // error handler
+  app.use((err, req, res, next) => {
+    res.status(400).send(err.message)
+  })
 
   
 
