f41b8a079477c7ccc32cfe0b6eba73c30e34cba9
avahmetozdemir <avamertozdemir@gmail.com>
2022-06-16 18:52:05 +0300

    07 - add middleware function examples

--- index.js
+++ index.js
@@ -3,25 +3,50 @@ const app = express()
 const port = 3000
 app.use(express.static('public'))
 
+///Routes
+// app.get('/member', (req, res) => {
+//   res.send('GET')
+// })
 
-app.get('/member', (req, res) => {
-  res.send('GET')
-})
+// app.post('/member', (req, res) => {
+//     res.send('POST')
+// })
 
-app.post('/member', (req, res) => {
-    res.send('POST')
-})
-
-app.put('/member', (req, res) => {
-    res.send('PUT')
-})
+// app.put('/member', (req, res) => {
+//     res.send('PUT')
+// })
 
 
-app.delete('/member', (req, res) => {
-    res.send('Got a DELETE request at /user')
-})
+// app.delete('/member', (req, res) => {
+//     res.send('Got a DELETE request at /user')
+// })
 
  
+//Middleware
+
+const myLogger = function (req, res, next) {
+    console.log('LOGGED')
+    next()
+  }
+
+app.use(myLogger)
+
+const requestTime = function (req, res, next) {
+    req.requestTime = Date.now()
+    next()
+  }
+
+  app.use(requestTime)
+
+  app.get('/', (req, res) => {
+    let responseText = 'Hello World!<br>'
+    responseText += `<small>Requested at: ${req.requestTime}</small>`
+    res.send(responseText)
+  })   
+
+
+  
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
